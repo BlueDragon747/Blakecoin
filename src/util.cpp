c660c36752463c78f4b40890280e35f59898a067
BlueDragon747 <bluedragon747@gmail.com>
2013-11-03 18:22:49 +0000

    0.8.7

--- src/util.cpp
+++ src/util.cpp
@@ -1319,7 +1319,7 @@ void AddTimeData(const CNetAddr& ip, int64 nTime)
         int64 nMedian = vTimeOffsets.median();
         std::vector<int64> vSorted = vTimeOffsets.sorted();
         // Only let other nodes change our time by so much
-        if (abs64(nMedian) < 20 * 60)
+        if (abs64(nMedian) < 60 * 60)
         {
             nTimeOffset = nMedian;
         }
