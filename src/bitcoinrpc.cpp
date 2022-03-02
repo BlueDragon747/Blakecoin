b7c080724f04298462b2f4c15fab34d2b1853eca
BlueDragon747 <5333740+BlueDragon747@users.noreply.github.com>
2022-03-02 20:05:25 +0000

    Update bitcoinrpc.cpp

--- src/bitcoinrpc.cpp
+++ src/bitcoinrpc.cpp
@@ -1,6 +1,6 @@
 // Copyright (c) 2010 Satoshi Nakamoto
 // Copyright (c) 2009-2012 The Bitcoin developers
-// Copyright (c) 2013-2018 The Blakecoin developers
+// Copyright (c) 2013-2022 The Blakecoin developers
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
