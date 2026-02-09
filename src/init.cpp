f82606f51245ec48ec3b50abfc7698931e511380
BlueDragon747 <5333740+BlueDragon747@users.noreply.github.com>
2026-02-09 03:57:00 +0000

    update
    url

--- src/init.cpp
+++ src/init.cpp
@@ -528,8 +528,8 @@ std::string HelpMessage(HelpMessageMode mode)
 
 std::string LicenseInfo()
 {
-    const std::string URL_SOURCE_CODE = "<https://github.com/bitcoin/bitcoin>";
-    const std::string URL_WEBSITE = "<https://bitcoincore.org>";
+    const std::string URL_SOURCE_CODE = "<https://github.com/BlueDragon747/Blakecoin>";
+    const std::string URL_WEBSITE = "<https://blakecoin.org>";
 
     return CopyrightHolders(strprintf(_("Copyright (C) %i-%i"), 2009, COPYRIGHT_YEAR) + " ") + "\n" +
            "\n" +
